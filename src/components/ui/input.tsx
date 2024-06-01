b7aee6b6b33e4e10ac54244696eb1521144aed88
mrevanzak <mrevanzak@gmail.com>
2024-06-01 13:42:46 +0700

    feat(ui): password toggle icon

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 import { cn } from '@/lib/utils';
 
@@ -6,16 +7,33 @@ export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
+    const [isVisible, setIsVisible] = React.useState(false);
+
     return (
-      <input
-        type={type}
-        className={cn(
-          'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-          className,
+      <div className='relative'>
+        <input
+          type={type === 'password' ? (isVisible ? 'text' : 'password') : type}
+          className={cn(
+            'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+            className,
+          )}
+          ref={ref}
+          {...props}
+        />
+        {type === 'password' && (
+          <button
+            className='absolute right-2 top-2.5 focus:outline-none'
+            type='button'
+            onClick={() => setIsVisible(!isVisible)}
+          >
+            {isVisible ? (
+              <FaEyeSlash className='pointer-events-none text-lg' />
+            ) : (
+              <FaEye className='pointer-events-none text-lg' />
+            )}
+          </button>
         )}
-        ref={ref}
-        {...props}
-      />
+      </div>
     );
   },
 );
