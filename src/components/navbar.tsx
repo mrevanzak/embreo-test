8eb59e7a6ec4a27bc39f2d101debafb1653104c0
mrevanzak <mrevanzak@gmail.com>
2024-06-01 15:28:20 +0700

    feat: navbar styling improvement

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,4 @@
-import {
-  CircleUser,
-  HeartHandshake,
-  Menu,
-  Package2,
-  Search,
-} from 'lucide-react';
+import { CircleUser, HeartHandshake, Search } from 'lucide-react';
 import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
@@ -17,7 +11,6 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Input } from '@/components/ui/input';
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 
 import { signOut } from '@/server/actions/auth';
 import { auth } from '@/server/auth';
@@ -27,7 +20,7 @@ export async function Navbar() {
 
   return (
     <header className='sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6'>
-      <nav className='hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6'>
+      <nav className='hidden flex-col gap-6 text-lg font-medium sm:flex sm:flex-row sm:items-center sm:gap-5 sm:text-sm lg:gap-6'>
         <Link
           href='#'
           className='flex items-center gap-2 text-lg font-semibold md:text-base'
@@ -42,59 +35,14 @@ export async function Navbar() {
           Dashboard
         </Link>
       </nav>
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button variant='outline' size='icon' className='shrink-0 md:hidden'>
-            <Menu className='h-5 w-5' />
-            <span className='sr-only'>Toggle navigation menu</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent side='left'>
-          <nav className='grid gap-6 text-lg font-medium'>
-            <Link
-              href='#'
-              className='flex items-center gap-2 text-lg font-semibold'
-            >
-              <Package2 className='h-6 w-6' />
-              <span className='sr-only'>Acme Inc</span>
-            </Link>
-            <Link href='#' className='hover:text-foreground'>
-              Dashboard
-            </Link>
-            <Link
-              href='#'
-              className='text-muted-foreground hover:text-foreground'
-            >
-              Orders
-            </Link>
-            <Link
-              href='#'
-              className='text-muted-foreground hover:text-foreground'
-            >
-              Products
-            </Link>
-            <Link
-              href='#'
-              className='text-muted-foreground hover:text-foreground'
-            >
-              Customers
-            </Link>
-            <Link
-              href='#'
-              className='text-muted-foreground hover:text-foreground'
-            >
-              Analytics
-            </Link>
-          </nav>
-        </SheetContent>
-      </Sheet>
+
       <div className='flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4'>
         <form className='ml-auto flex-1 sm:flex-initial'>
           <div className='relative'>
-            <Search className='absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground' />
+            <Search className='absolute left-2.5 top-2.5 h-6 w-6 text-muted-foreground' />
             <Input
               type='search'
-              placeholder='Search products...'
+              placeholder='Search event...'
               className='pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]'
             />
           </div>
